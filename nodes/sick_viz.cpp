737932cbb6d71b8468bf14dfb851c1f7145f31fb
joshf <jfriesen@locusrobotics.com>
2024-04-29 19:44:55 +0000

    progress update

--- nodes/sick_viz.cpp
+++ nodes/sick_viz.cpp
@@ -6,21 +6,21 @@
 
 class SafetyFieldVisualizer {
 public:
-    SafetyFieldVisualizer(const std::string& robot, const std::string& laser, int augment = 0)
-        : robot_(robot), laser_(laser), augment_(augment) {
+    SafetyFieldVisualizer(const std::string& robot, const std::string& laser, bool dtz = false)
+        : robot_(robot), laser_(laser), dtz_(dtz) {
         // Wait for the service to get the field data
         ros::service::waitForService("/" + robot + "/" + laser + "_nanoscan/field_data", ros::Duration(5));
         field_data_client_ = nh_.serviceClient<sick_safetyscanners::FieldData>("/" + robot + "/" + laser + "_nanoscan/field_data");
         field_data_client_.call(field_data_);
-        zone_type_ = (augment) ? "protective":"DTZ";
+        zone_type_ = (dtz) ? "DTZ":"protective";
 
         ROS_INFO_STREAM("Number of fields: " << field_data_.response.fields.size());
         safety_field_pub_ = nh_.advertise
         <sensor_msgs::LaserScan>("/" + robot + "/" + laser + "_nanoscan/safety_field/" + zone_type_, 10);
         current_safety_field_.header.frame_id = robot + "/" + laser + "_laser_link";
-        current_safety_field_.angle_min = field_data_.response.fields[augment].start_angle;
-        current_safety_field_.angle_max = field_data_.response.fields[augment].start_angle + field_data_.response.fields[augment].angular_resolution * field_data_.response.fields[augment].ranges.size();
-        current_safety_field_.angle_increment = field_data_.response.fields[augment].angular_resolution;
+        current_safety_field_.angle_min = field_data_.response.fields[dtz].start_angle;
+        current_safety_field_.angle_max = field_data_.response.fields[dtz].start_angle + field_data_.response.fields[dtz].angular_resolution * field_data_.response.fields[dtz].ranges.size();
+        current_safety_field_.angle_increment = field_data_.response.fields[dtz].angular_resolution;
         // current_safety_field_.ranges = field_data.response.fields[augment].ranges;
         current_safety_field_.range_min = 0.01;
         current_safety_field_.range_max = 8.0;
@@ -31,7 +31,10 @@ public:
 
     void microscanCallback(const sick_safetyscanners::OutputPathsMsg::ConstPtr& msg) {
         current_safety_field_.header.stamp = ros::Time::now();
-        current_safety_field_.ranges = field_data_.response.fields[msg->active_monitoring_case + augment_].ranges;
+        current_safety_field_.ranges = field_data_.response.fields[field_data_.response.monitoring_cases[msg->active_monitoring_case].fields[dtz_]].ranges;
+        // current_safety_field_.ranges = field_data_.response.fields[0].ranges;
+        ROS_INFO_STREAM("Here: " << msg->active_monitoring_case);
+        ROS_INFO_STREAM("Here2: " << field_data_.response.monitoring_cases[msg->active_monitoring_case-1].fields[dtz_]);
         safety_field_pub_.publish(current_safety_field_);
     }
 
@@ -43,7 +46,7 @@ private:
     sensor_msgs::LaserScan current_safety_field_;
     std::string robot_;
     std::string laser_;
-    int augment_;
+    bool dtz_;
     std::string zone_type_;
     sick_safetyscanners::FieldData field_data_;
 };
@@ -51,13 +54,13 @@ private:
 int main(int argc, char** argv) {
     ros::init(argc, argv, "sick_viz");
 
-    std::string robot_name = "v2_19813";
-    SafetyFieldVisualizer left_protective(robot_name, "left");
-    SafetyFieldVisualizer right_protective(robot_name, "right");
-    SafetyFieldVisualizer rear_protective(robot_name, "rear");
-    SafetyFieldVisualizer left_DTZ(robot_name, "left", 1);
-    SafetyFieldVisualizer right_DTZ(robot_name, "right", 1);
-    SafetyFieldVisualizer rear_DTZ(robot_name, "rear", 1);
+    std::string robot_name = "v2_19811";
+    SafetyFieldVisualizer left_protective(robot_name, "left", false);
+    SafetyFieldVisualizer right_protective(robot_name, "right", false);
+    SafetyFieldVisualizer rear_protective(robot_name, "rear", false);
+    SafetyFieldVisualizer left_DTZ(robot_name, "left", true);
+    SafetyFieldVisualizer right_DTZ(robot_name, "right", true);
+    SafetyFieldVisualizer rear_DTZ(robot_name, "rear", true);
 
     ros::spin();
 
