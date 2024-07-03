d2ecfce405ae09701ecb3e51c3fcb054cfff8a27
Josh Friesen <jfriesen@locusrobotics.com>
2024-07-03 22:06:03 +0000

    cleanup

--- include/sick_viz/SickViz.h
+++ include/sick_viz/SickViz.h
@@ -23,7 +23,6 @@ public:
 
 private:
     void preprocessFieldData();
-    void simplifyMarkerPoints(visualization_msgs::Marker& marker, std::size_t polygon_size)
 
     ros::NodeHandle nh_;
     ros::ServiceClient field_data_client_;
